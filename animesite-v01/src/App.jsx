a3ae101810f77e5ab2859cf51dde5049b5a3fb85
RoronoaZoro10 <revan28710@gmail.com>
2025-11-29 17:58:33 +0530

    added bootstrap elements

--- animesite-v01/src/App.jsx
+++ animesite-v01/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div className="tempdiv mx-5 p-5 border">
+        <p className="text-info text-decoration-underline text-center"><i class="bi bi-person-circle"></i>temp</p>
+      </div>
     </>
   )
 }
